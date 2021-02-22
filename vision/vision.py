e0b6e91f42fdf7f47158e9eac70264bbc61d3660
XDGFX <callum.morrison@mac.com>
2021-02-22 21:56:00 +0000

    Added movement correction based on image tracking

--- vision/vision.py
+++ vision/vision.py
@@ -8,6 +8,7 @@ Callum Morrison, 2021
 """
 
 import json
+import math
 import os
 import re
 import time
@@ -38,9 +39,12 @@ r = redis.Redis(host=redis_url[0], port=redis_url[1], db=0)
 class Camera():
     def __init__(self, sid):
         self.movement_key = f'movement--{sid}'
-        self.cam = cv2.VideoCapture(0)
-        # self.cam = cv2.VideoCapture(str(
-        #     Path(__file__).parent.absolute() / '0000.mkv'))
+
+        # self.cam = cv2.VideoCapture(0)
+
+        self.cam = cv2.VideoCapture(str(
+            Path(__file__).parent.absolute() / '0000.mkv'))
+
         self.active = False
 
         self.frame_buffer = Queue()
@@ -65,6 +69,7 @@ class Camera():
         self.active = True
 
         start_time = time.time()
+        frame_wait = 1 / int(os.getenv('FRAMERATE_TRACK'))
 
         while self.active:
             ret, frame = self.cam.read()
@@ -80,7 +85,6 @@ class Camera():
                 self.frame_buffer.put(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
             # Wait until next frame should be captured
-            frame_wait = 1 / int(os.getenv('FRAMERATE_TRACK'))
             time.sleep(frame_wait - ((time.time() - start_time) % frame_wait))
 
     def start_track(self):
@@ -157,34 +161,149 @@ class Camera():
                 log.debug('Failed to calculate transformation', e)
                 self.track_err_count += 1
 
+    def get_inference(self, img):
+        """
+        Send an image to the inference server and return the result.
 
-def get_inference(img):
-    """
-    Send an image to the inference server and return the result.
+        @return
+        bbox        json list of bounding boxes
+        """
+        url = os.getenv('INFERENCE_URL') + '/api/detect'
 
-    @return
-    bbox        json list of bounding boxes
-    """
-    url = os.getenv('INFERENCE_URL') + '/api/detect'
+        # Prepare headers for http request
+        headers = {'content-type': 'image/jpeg'}
 
-    # Prepare headers for http request
-    headers = {'content-type': 'image/jpeg'}
+        # Encode image as jpeg
+        _, img_encoded = cv2.imencode('.jpg', img)
 
-    # Encode image as jpeg
-    _, img_encoded = cv2.imencode('.jpg', img)
+        # Send http request with image and receive response
+        try:
+            response = requests.post(
+                url,
+                data=img_encoded.tostring(),
+                headers=headers,
+                timeout=int(os.getenv('INFERENCE_TIMEOUT')) / 1000
+            )
 
-    # Send http request with image and receive response
-    try:
-        response = requests.post(
-            url,
-            data=img_encoded.tostring(),
-            headers=headers,
-            timeout=int(os.getenv('INFERENCE_TIMEOUT')) / 1000
-        )
+            return json.loads(response.text)
 
-        return json.loads(response.text)
+        except requests.exceptions.ReadTimeout as e:
+            log.error(e)
 
-    except requests.exceptions.ReadTimeout as e:
-        log.error(e)
+            return None
 
-        return None
+
+class Servo():
+    def __init__(self, sid, img_width, img_height):
+        self.movement_key = f'movement--{sid}'
+        self.img_width = img_width
+        self.img_height = img_height
+
+    def print_movement(self):
+        """
+        Prints the current movement position
+        """
+        print(json.loads(r.get(self.movement_key)))
+
+    def correct_bbox(self, bbox):
+        """
+        Correct a list of bounding boxes according to the current movement
+
+        bbox        list of bounding boxes
+
+        # return
+        bbox        corrected list of bounding boxes
+        """
+
+        # Check and correct input values
+        assert self.img_width == round(self.img_width)
+        assert self.img_height == round(self.img_height)
+
+        # For reference, current_movement is in the form:
+        #   (x, y, a),
+        # where +ve x, y is when the camera moves up, left respectively,
+        #   and +ve a is when the camera moves anti-clockwise
+        current_movement = json.loads(r.get(self.movement_key))
+
+        for i in range(bbox['count']):
+            box = bbox['bounding_boxes'][i]
+
+            # Extract coordinates from input
+            x, y = self.bbox2centre(box)
+            w = box[2]
+            h = box[3]
+
+            # Convert centre points based on movement
+            x = x + current_movement[0]
+            y = y + current_movement[1]
+
+            # Convert centre points based on rotation
+            a = current_movement[2]
+            rho, phi = self.cart2pol(x, y)
+            x, y = self.pol2cart(rho, phi - a)
+
+            # Convert w, h based on rotation
+            w = w * math.cos(a) + h * math.sin(a)
+            h = h * math.cos(a) + w * math.sin(a)
+
+            # Convert back to bbox format
+            new_bbox = self.centre2bbox(x, y, w, h)
+
+            # Assign back into original dictionary
+            bbox['bounding_boxes'][i] = new_bbox
+
+        return bbox
+
+    def bbox2centre(self, bbox):
+        """
+        Converts a bounding box in the form (x, y, w, h) to a centre point (x, y),
+        while also moving coordinates from bbox origin (top, left) to movement origin (centre)
+        """
+
+        # Convert to centre point
+        x = bbox[0] + bbox[2] / 2
+        y = bbox[1] + bbox[3] / 2
+
+        # Convert to movement coordinates
+        correct_x = self.img_width / 2
+        correct_y = self.img_height / 2
+        x = x - correct_x
+        y = y - correct_y
+
+        return (x, y)
+
+    def centre2bbox(self, x, y, w, h):
+        """
+        Converts a centre point in the form (x, y) to a bounding box (x, y, w, h),
+        while also moving coordinates from movement origin (centre), to bbox origin (top, left)
+        """
+
+        # Convert to bbox point
+        x = x - w / 2
+        y = y - h / 2
+
+        # Convert to movement coordinates
+        correct_x = self.img_width / 2
+        correct_y = self.img_height / 2
+        x = x + correct_x
+        y = y + correct_y
+
+        return (x, y, w, h)
+
+    def cart2pol(self, x, y):
+        """
+        Convert from cartesian coordinates to polar coordinates.
+        """
+        rho = np.sqrt(x**2 + y**2)
+        phi = np.arctan2(y, x)
+
+        return(rho, phi)
+
+    def pol2cart(self, rho, phi):
+        """
+        Convert from polar coordinates to cartesian coordinates.
+        """
+        x = rho * np.cos(phi)
+        y = rho * np.sin(phi)
+
+        return(x, y)
