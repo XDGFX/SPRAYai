cef301a889332b11898f47bc5fc81ca579c5352b
XDGFX <callum.morrison@mac.com>
2021-02-22 13:42:52 +0000

    Now using namespaces

--- control/app/app.py
+++ control/app/app.py
@@ -8,11 +8,13 @@ Flask-based API webserver to handle control over the Navvy smart sprayer.
 Callum Morrison, 2021
 """
 
-from flask import Flask, request
-from flask_socketio import SocketIO, send, emit
+import threading
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO
 from threading import Thread
 
 app = Flask(__name__)
+# sio = SocketIO(app, async_mode='threading')
 sio = SocketIO(app)
 
 # Client_list is a big list of all connected clients, and includes information about the client.
@@ -21,21 +23,51 @@ client_list = []
 # Connections is a dictionary in the form {sid: index} where `index` is the location of the client in client_list with sid=sid
 connections = {}
 
+valid_namespaces = ["/pi", "/host", "/"]
+
+
 def serve():
-    sio.run(app, host="0.0.0.0", port="5040")
+    sio.run(app, host='0.0.0.0', port='5040')
+
 
 def start_server():
     thread = Thread(target=serve)
     thread.start()
-    print("Webserver started")
+    print('Webserver started')
+
 
 # --- WEBSERVER ROUTES ---
 @app.route('/')
 def index():
-    return 'Control system webserver is working!'
+    return render_template('index.html')
+
+
+@app.route('/emit/<namespace>')
+def emit_pi(namespace=None):
+    """
+    Emit custom command to a specific namespace
+    REST API version
+    """
+    cmd = request.args.get('cmd')
+
+    # Validate request
+    if not (cmd and namespace):
+        return ('Missing command or namespace', 400)
+
+    # Add leading slash
+    namespace = f"/{namespace}"
+
+    if namespace not in valid_namespaces:
+        return (f'Invalid namespace: {namespace}', 405)
+
+    print(f'Received emit request: {cmd} for namespace {namespace}')
+    sio.emit(cmd, namespace=namespace)
+
+    return ('', 200)
+
 
 # --- WEBSOCKET ROUTES ---
-@sio.on('register_client')
+@sio.event(namespace='/pi')
 def register_client(client):
 
     # Register the client in the client_list
@@ -43,17 +75,54 @@ def register_client(client):
     client_list.append(client)
     connections[request.sid] = index
 
+    print(client_list)
 
-@sio.event
+    print(f'Registered client: {client}')
+
+
+@sio.event(namespace='/pi')
 def connect():
-    print(request.sid)
-    print("Something connected!")
+    print(f'{request.sid} just connected to /pi')
 
-@sio.event
+
+@sio.event(namespace='/pi')
 def disconnect():
-    print(f"{request.sid} has disconnected")
+    print(f'{request.sid} has disconnected from /pi')
 
     # Remove that client from the client_list
     index = connections[request.sid]
     del connections[request.sid]
     del client_list[index]
+
+
+@sio.event(namespace='/host')
+def connect():
+    print(f'{request.sid} just connected to /host')
+
+
+@sio.event(namespace='/host')
+def disconnect():
+    print(f'{request.sid} has disconnected from /host')
+
+
+@sio.event(namespace='/host')
+def emit(data):
+    """
+    Emit custom command to a specific namespace
+    WebSocket version
+    """
+    # Validate request
+    cmd = data['cmd']
+    namespace = data['namespace']
+
+    if not (cmd and namespace):
+        print('Missing command or namespace')
+
+    # Add leading slash
+    namespace = f"/{namespace}"
+
+    if namespace not in valid_namespaces:
+        print(f'Invalid namespace: {namespace}')
+
+    print(f'Received emit request: {cmd} for namespace {namespace}')
+    sio.emit(cmd, namespace=namespace)
