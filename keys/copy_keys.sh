54897f70227158f13e49db5629d7d7dce1b92821
Callum <callum.morrison@mac.com>
2021-03-04 10:20:16 +0000

    Move ansible playbook

--- keys/copy_keys.sh
+++ keys/copy_keys.sh
@@ -1,1 +1,1 @@
-cat navvy_rsa.pub | ssh ubuntu@192.168.1.161 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys"
+cat navvy_rsa.pub | ssh pi@192.168.0.29 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys"
