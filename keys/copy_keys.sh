ebb3fec9168ae6c5d8ac876c1ab929a3d784bb45
Callum <callum.morrison@mac.com>
2021-03-05 12:46:25 +0000

    Working ansible for Jetson

--- keys/copy_keys.sh
+++ keys/copy_keys.sh
@@ -1,1 +1,8 @@
-cat navvy_rsa.pub | ssh pi@192.168.0.29 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys"
+#!/usr/bin/bash
+
+# Copy public key to new host.
+# Argument in the form user@192.168.0.x
+# e.g. `./copy_keys.sh nxnx@192.168.0.10`
+
+
+cat navvy_rsa.pub | ssh $1 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys"
